94ec8a987ee1b43cbc38153277a2fe7780e58658
_x9l7n <nickethachin@gmail.com>
2017-12-20 23:12:00 +0700

    v0.00.02

--- bot.php
+++ bot.php
@@ -20,14 +20,21 @@ if (!is_null($events['events'])) {
 			$messages = [
 				'type' => 'text',
 				'text' => $text
-			];
+            ];
+
+            if($text=='!code'){
+                $messages = [
+                    'type' => 'text',
+                    'text' => '##street ##gangster ##train'
+            };
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
-			];
+            ];
+
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
